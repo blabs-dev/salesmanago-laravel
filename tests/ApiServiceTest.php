284e2b8354d84afb55411afb0e7e917148800624
Salvo Bonanno <salvob.labs@gmail.com>
2023-01-04 14:22:03 +0100

    Refactors test class

--- tests/ApiServiceTest.php
+++ tests/ApiServiceTest.php
@@ -3,7 +3,7 @@
 use Blabs\SalesManago\Services\ApiService;
 use Blabs\SmLaravel\SmLaravel;
 
-it('can test', function () {
+it('can instantiate sales manago api service', function () {
     $service = app(SmLaravel::class);
     expect(get_class($service->service))->toBe(ApiService::class);
 });
